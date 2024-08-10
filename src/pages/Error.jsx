0f8018765e15a53fc8f8425d3346b3f7ccc66cfc
Mohib Hussain <146108360+mohib990T@users.noreply.github.com>
2024-08-10 13:01:39 +0530

    Update Error.jsx

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -1,6 +1,17 @@
+import { Link } from "react-router-dom";
+
 const Error = () => {
   return (
-    <div>Error</div>
-  )
-}
-export default Error
+    <>
+      <div className="text-4xl text-red-700 flex flex-col items-center justify-center mt-20">
+        <div>Error : Page not found...</div>
+        <Link to="/">
+          <button className="text-lg bg-green-500 text-white font-semibold mt-10 px-5 py-2 rounded-full hover:bg-green-600">
+            Go to Home Page
+          </button>
+        </Link>
+      </div>
+    </>
+  );
+};
+export default Error;
